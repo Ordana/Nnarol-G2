217d100814fa5559b53e65a127eea94a762942a6
Ikramsrr <33417689+Ikramsrr@users.noreply.github.com>
2018-05-30 13:12:17 +0200

    Add files via upload
    Map

---
+++ Level_4.sql
@@ -0,0 +1,251 @@
+INSERT INTO element VALUES (null,"cross-wall",0,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",1,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",2,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",3,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",4,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",5,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",6,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",7,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",8,0,null,4);
+INSERT INTO element VALUES (null,"cross-wall",9,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",10,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",11,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",12,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",13,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",14,0,null,4);
+INSERT INTO element VALUES (null,"cross-wall",15,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",16,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",17,0,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",18,0,null,4);
+INSERT INTO element VALUES (null,"cross-wall",19,0,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",0,1,null,4);
+INSERT INTO element VALUES (null,"hero",1,1,"BOTTOMRIGHT",4);
+INSERT INTO element VALUES (null,"vertical-wall",10,1,null,4);
+INSERT INTO element VALUES (null,"monster",15,1,"RIGHT",4);
+INSERT INTO element VALUES (null,"vertical-wall",19,1,null,4);
+INSERT INTO element VALUES (null,"cross-wall",0,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",1,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",2,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",3,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",4,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",5,2,null,4);
+INSERT INTO element VALUES (null,"cross-wall",8,2,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",9,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",10,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",11,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",12,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",13,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",14,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",15,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",16,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",17,2,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",18,2,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",19,2,null,4);
+INSERT INTO element VALUES (null,"cross-wall",0,3,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",1,3,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",1,3,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",2,3,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",3,3,null,4);
+INSERT INTO element VALUES (null,"cross-wall",7,3,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",8,3,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",9,3,null,4);
+INSERT INTO element VALUES (null,"cross-wall",15,3,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",16,3,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",17,3,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",18,3,null,4);
+INSERT INTO element VALUES (null,"cross-wall",19,3,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",0,4,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",1,4,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",2,4,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",3,4,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",4,4,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",5,4,null,4);
+INSERT INTO element VALUES (null,"life-buble",13,4,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",19,4,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",0,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",2,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",3,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",4,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",5,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",6,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",7,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",8,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",9,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",10,5,null,4);
+INSERT INTO element VALUES (null,"cross-wall",15,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",16,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",17,5,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",18,5,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",19,5,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",0,6,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",8,6,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",9,6,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",10,6,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",19,6,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",0,7,null,4);
+INSERT INTO element VALUES (null,"cross-wall",4,7,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",5,7,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",10,7,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",11,7,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",12,7,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",13,7,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",14,7,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",15,7,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",19,7,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",0,8,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",4,8,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",5,8,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",6,8,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",7,8,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",19,8,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",0,9,null,4);
+INSERT INTO element VALUES (null,"cross-wall",1,9,null,4);
+INSERT INTO element VALUES (null,"cross-wall",7,9,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",8,9,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",9,9,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",10,9,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",11,9,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",12,9,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",15,9,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",16,9,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",18,9,null,4);
+INSERT INTO element VALUES (null,"cross-wall",19,9,null,4);
+INSERT INTO element VALUES (null,"door",0,10,"RIGHT",4);
+INSERT INTO element VALUES (null,"RIGHT",4,10,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",8,10,null,4);
+INSERT INTO element VALUES (null,"life-buble",11,10,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",16,10,null,4);
+INSERT INTO element VALUES (null,"vertical-wall",19,10,null,4);
+INSERT INTO element VALUES (null,"cross-wall",0,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",1,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",2,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",3,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",4,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",5,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",6,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",7,11,null,4);
+INSERT INTO element VALUES (null,"cross-wall",8,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",9,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",10,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",11,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",12,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",13,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",14,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",15,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",16,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",17,11,null,4);
+INSERT INTO element VALUES (null,"horizontal-wall",18,11,null,4);
+INSERT INTO element VALUES (null,"cross-wall",19,11,null,4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+INSERT INTO element VALUES (null,"cross-wall",0,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",1,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",2,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",3,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",4,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",5,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",6,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",7,11,2,2);
+INSERT INTO element VALUES (null,"cross-wall",8,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",9,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",10,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",11,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",12,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",13,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",14,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",15,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",16,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",17,11,2,2);
+INSERT INTO element VALUES (null,"horizontal-wall",18,11,2,2);
+INSERT INTO element VALUES (null,"cross-wall",19,11,2,2);
