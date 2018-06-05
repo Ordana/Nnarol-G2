13a5b3dd20f0cd9da4eb83ec5dc724edef96dee5
douniaben <dounia.benmerar.dz@viacesi.fr>
2018-06-05 13:51:11 +0100

    Run as Java application

---
+++ main/src/main/java/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import controller.Controller;
+import model.Model;
+import view.View;
+
+/**
+ * The Class Main
+ *
+ * @author Rafik Belkadi
+ */
+public abstract class Main {
+
+	/**
+	 * The main method
+	 *
+	 * @param args
+	 *          the arguments
+	 */
+	public static void main(final String[] args) {
+		Model model = new Model();
+		View view = new View();
+		Controller controller = new Controller(view,model);
+		view.setController(controller);
+		controller.start();
+	}
+}
